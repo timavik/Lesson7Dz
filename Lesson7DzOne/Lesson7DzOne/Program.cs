88398300f004de512e987f6118d55b016b434eb0
timavik <103438439+timavik@users.noreply.github.com>
2024-12-30 14:20:15 +0300

    Add files via upload

---
+++ Lesson7DzOne/Lesson7DzOne/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lesson7DzOne
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] numbers = new int[6];
+
+      Console.WriteLine("Введите 6 целых чисел:");
+      for (int i = 0; i < numbers.Length; i++)
+      {
+        numbers[i] = int.Parse(Console.ReadLine());
+      }
+
+      Array.Sort(numbers);
+      Array.Reverse(numbers);
+
+      Console.WriteLine("Отсортированные по убыванию элементы массива:");
+      foreach (int num in numbers)
+      {
+        Console.WriteLine(num);
+      }
+    }
+  }
+}
---
+++ Lesson7DzTwo/Lesson7DzTwo/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Lesson7DzTwo
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+
+      int[,] matrix = {
+            { 5, 8, 3 },
+            { 10, 2, 7 },
+            { 4, 6, 9 }
+        };
+
+
+      Console.WriteLine("Максимальные элементы каждого ряда:");
+      for (int i = 0; i < 3; i++)
+      {
+        int maxElement = matrix[i, 0]; 
+        for (int j = 1; j < 3; j++)
+        {
+          if (matrix[i, j] > maxElement)
+          {
+            maxElement = matrix[i, j];
+          }
+        }
+        Console.WriteLine($"Ряд {i + 1}: {maxElement}");
+      }
+    }
+  }
+}
